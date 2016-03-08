bd24a39496486b671a9814209521544c3042ef8e
psistorma <psistorm@126.com>
2016-03-08 08:49:37 +0800

    improve

--- UserPlugins/SublimeUtils/Panel.py
+++ UserPlugins/SublimeUtils/Panel.py
@@ -1,8 +1,39 @@
+import functools as ft
 import sublime
 
-def showInputPanel(window, onDone, title="", initText="", *, onChange=None, onCancel=None):
-    if not window:
-        window = sublime.active_window()
+def fwShowQuickPanel(on_done, on_highlighted=None, timeout=10):
+    def decorator(f):
+        @ft.wraps(f)
+        def wrapper(*args, **kwds):
+            items, selected_index, *flags = f(*args, **kwds)
+            if flags:
+                flags, = flags
 
+            showQuickPanel(None, items, on_done, timeout,
+                           on_highlighted=on_highlighted, selected_index=selected_index)
+
+        return wrapper
+
+    return decorator
+
+def _fwPrepareWindow(f):
+    @ft.wraps(f)
+    def wapper(window, *args, **kwds):
+        if not window:
+            window = sublime.active_window()
+
+        return f(window, *args, **kwds)
+
+    return wapper
+
+@_fwPrepareWindow
+def showInputPanel(window, onDone, title="", initText="", *, onChange=None, onCancel=None):
     window.show_input_panel(
         title, initText, onDone, onChange, onCancel)
+
+@_fwPrepareWindow
+def showQuickPanel(window, items, on_done, timeout=10, **kwds):
+    if timeout:
+        sublime.set_timeout(lambda: window.show_quick_panel(items, on_done, **kwds), timeout)
+    else:
+        window.show_quick_panel(items, on_done, **kwds)
